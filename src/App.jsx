d8b0dd8ec3ad44294bda621154e3b2e4761a391b
Katie Bowers <katiebowers0210@gmail.com>
2023-07-24 14:10:37 -0400

    used data to display puppy name in div

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { puppyList } from './data.js'
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  
+//useState returns an array, the array contains 2 elements 
+//first is the value you're storing
+//second is a function that can be used to reset the value
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log('puppyList: ', puppyList);
   return (
     <>
       <div>
-        
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
-  )
+  );
 }
 
 export default App
